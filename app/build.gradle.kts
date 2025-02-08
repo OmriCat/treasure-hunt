497c0f8c59529e2bc613fc49675fce6f5a74bbfd
Joseph Cooper <cooper.joseph@gmail.com>
2025-02-08 16:09:25 +0000

    chore: Add template project

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.kotlin.compose)
+}
+
+val javaVersion = 22
+
+android {
+  namespace = "com.omricat.treasurehunt"
+  compileSdk = 35
+
+  defaultConfig {
+    applicationId = "com.omricat.treasurehunt"
+    minSdk = 24
+    targetSdk = 35
+    versionCode = 1
+    versionName = "0.1"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.toVersion(javaVersion)
+    targetCompatibility = JavaVersion.toVersion(javaVersion)
+  }
+  kotlinOptions { jvmTarget = "$javaVersion" }
+  buildFeatures { compose = true }
+}
+
+dependencies {
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.ui)
+  implementation(libs.androidx.ui.graphics)
+  implementation(libs.androidx.ui.tooling.preview)
+  implementation(libs.androidx.material3)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.androidx.ui.test.junit4)
+  debugImplementation(libs.androidx.ui.tooling)
+  debugImplementation(libs.androidx.ui.test.manifest)
+}
