79dd4834be8b2e0ebe388267806e51ae468fccf8
Joseph Cooper <cooper.joseph@gmail.com>
2025-02-09 13:28:26 +0000

    build: Introduce Gradle doctor plugin and tweak properties
    The Gradle Doctor plugin helps ensure that the CLI and IDE are both using
    the same Java and that we've got decent gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+  plugins { id("com.osacky.doctor") version "0.10.0" }
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.kotlin.compose) apply false
 }
