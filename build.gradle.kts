b40b123b00304340ae35653ee4fd029d4a49caad
Joseph Cooper <cooper.joseph@gmail.com>
2025-02-11 15:39:55 +0000

    refactor: Move gradle doctor plugin to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  plugins { id("com.osacky.doctor") version "0.12.1" }
+  alias(libs.plugins.gradleDoctor)
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.compose) apply false
