a2f6e7b60c3aad03f1e06eaabbf17f08c2e0df46
Joseph Cooper <cooper.joseph@gmail.com>
2025-10-01 12:40:40 +0100

    test: Update tests

--- app/src/test/kotlin/com/omricat/treasurehunt/AppStateTest.kt
+++ app/src/test/kotlin/com/omricat/treasurehunt/AppStateTest.kt
@@ -10,18 +10,4 @@ class AppStateTest {
 
         assert(state.clues.all { clue -> clue.solved.not() })
     }
-
-    @Test
-    fun solvingLastClueOfRoundReturnsNextRound() {
-        val state =
-            HuntState.Round(roundId = RoundId.ONE, clues = setOf(Clue(ClueId.WASHING_MACHINE)))
-
-        val newState = state.trySolveClue(ClueId.WASHING_MACHINE)
-
-        assert(newState is HuntState.SolveResult.Success)
-        assert(
-            ((newState as HuntState.SolveResult.Success).nextHuntState as HuntState.Round)
-                .roundId == RoundId.TWO
-        )
-    }
 }
